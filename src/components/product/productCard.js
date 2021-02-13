3fcdfa150dab2367458c368eed725ff475dba91c
sean-one <basement365@gmail.com>
2021-02-12 16:28:38 -0800

    created a product card for singular styling

--- src/components/product/productCard.js
+++ src/components/product/productCard.js
@@ -3,13 +3,13 @@ import React from 'react';
 import './productCard.css';
 
 const productCard = (props) => {
-    
     return (
         <div className='productLine'>
             <div className="productImg">
                 <img src={`https://via.placeholder.com/250x300?text=${props.product.item}`} alt={`stock link to represent ${props.product.item}`} />
             </div>
             <div className="productDetails">
+                {/* {detailLine} */}
                 <h4>{props.product.item} | ${props.product.item_price}</h4>
                 <p><em>{props.product.item_description}</em></p>
             </div>
--- src/components/product/products.js
+++ src/components/product/products.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import AxiosInstance from '../../helpers/axios';
 
+import ProductCard from '../product/productCard';
+
 import './products.css';
 
 const Products = () => {
     const [productList, getProductList] = useState([]);
-    const [updatedProductList, getUpdatedProductList] = useState([]);
+    // const [updatedProductList, getUpdatedProductList] = useState([]);
 
     useEffect(() => {
         AxiosInstance.get('/products')
@@ -15,41 +17,25 @@ const Products = () => {
                 return res;
             })
             .catch(err => console.log(err));
-    }, [updatedProductList]);
+            // if adding adjustProduct add "updatedProductList" to array
+    }, []);
 
-    const adjustProduct = async (e, adjustment, id) => {
-        e.preventDefault();
-        await AxiosInstance.put(`/products/${adjustment}/${id}`, { amount: 1 })
-            .then(res => {
-                const productIndex = productList.findIndex(product => product.id === id)
-                productList[productIndex].item_inventory = res.data[0].item_inventory
-                getUpdatedProductList(productList);
-                return productList;
-            })
-            .catch(err => console.log(err))
-    }
+    // const adjustProduct = async (e, adjustment, id) => {
+    //     e.preventDefault();
+    //     await AxiosInstance.put(`/products/${adjustment}/${id}`, { amount: 1 })
+    //         .then(res => {
+    //             const productIndex = productList.findIndex(product => product.id === id)
+    //             productList[productIndex].item_inventory = res.data[0].item_inventory
+    //             getUpdatedProductList(productList);
+    //             return productList;
+    //         })
+    //         .catch(err => console.log(err))
+    // }
 
     return (
         <div className='productsPage'>
             {productList.map((product, i) => (
-                <div className='product' key={product.id}>
-                    <div className='productImg'>
-                        <img src={`https://via.placeholder.com/250x300?text=${product.item}`} alt={`stock link to represent ${product.item}`} />
-                    </div>
-                    <div className='productDetails'>
-                        <div className='productTitle'>{product.item}</div>
-                        <div className='productDescription'>{product.item_description}</div>
-                    </div>
-                    <div className='inventory'>
-                        <div className='inventoryItem productAdjust'>
-                            <button name={product.id} onClick={e => adjustProduct(e, 'removeProduct', product.id)}>-</button>
-                        </div>
-                        <div className='inventoryItem currentStock' key={product.id}>{product.item_inventory}</div>
-                        <div className='inventoryItem productAdjust'>
-                            <button name={product.id} onClick={e => adjustProduct(e, 'addProduct', product.id)}>+</button>
-                        </div>
-                    </div>
-                </div>
+                <ProductCard key={product.id} product={product} />
             ))}
         </div>
     )
