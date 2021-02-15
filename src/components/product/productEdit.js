b74706a345c33cd8fed8c5f3281eee9dc759f0da
SeanOne <basement365@gmail.com>
2021-02-14 22:30:38 -0800

    working on product edit card

--- src/components/product/productEdit.js
+++ src/components/product/productEdit.js
@@ -6,8 +6,6 @@ import AxiosInstance from '../../helpers/axios';
 const ProductEdit = () => {
     let { url } = useRouteMatch()
     const [productDetails, setProductDetails] = useState()
-    // console.log(path)
-    // console.log(url)
     
     useEffect(() => {
         AxiosInstance.get(`${url}`)
@@ -15,12 +13,26 @@ const ProductEdit = () => {
                 setProductDetails(res.data)
             })
             .catch(err => console.log(err));
-    }, []);
-
+    }, [url]);
+    console.log(productDetails)
+    let editProduct;
+    if (productDetails === undefined) {
+        editProduct = <div>'Loading...'</div>
+    } else {
+        editProduct = 
+            <form className="editProductLine">
+                <input type="text" name="item" value={'Product Title' || productDetails.item} />
+                <input type="text" name="description" value={'Product Description' || productDetails.item_description} />
+                <input type="number" name="price" step="0.01" value={0 || productDetails.item_price} />
+                <input type="number" name="inventory" value={0 || productDetails.item_inventory} />
+            </form>
+    }
+    
+    {/* id: 2, item: "killer cold brew", item_description: "cafecito coffe poured over ice", item_price: 10, item_inventory: 9 */}
     return (
-        <div>
-            this is the edit card
-        </div>
+        <>
+            {editProduct}
+        </>
     );
 };
 
